059b0fa893e18f81ab2dec06420cfc5fc34aa6e7
Varun Sharma <varunsharmalive@gmail.com>
2017-04-23 22:35:32 +0530

    Change sequences version.

--- docs/sequences.js
+++ docs/sequences.js
@@ -28,19 +28,20 @@ var vis = d3.select("#chart").append("svg:svg")
     .attr("id", "container")
     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-var partition = d3.partition()
-    .size([2 * Math.PI, radius * radius]);
+var partition = d3.layout.partition()
+    .size([2 * Math.PI, radius * radius])
+    .value(function(d) { return d.size; });
 
-var arc = d3.arc()
-    .startAngle(function(d) { return d.x0; })
-    .endAngle(function(d) { return d.x1; })
-    .innerRadius(function(d) { return Math.sqrt(d.y0); })
-    .outerRadius(function(d) { return Math.sqrt(d.y1); });
+var arc = d3.svg.arc()
+    .startAngle(function(d) { return d.x; })
+    .endAngle(function(d) { return d.x + d.dx; })
+    .innerRadius(function(d) { return Math.sqrt(d.y); })
+    .outerRadius(function(d) { return Math.sqrt(d.y + d.dy); });
 
-// Use d3.text and d3.csvParseRows so that we do not need to have a header
+// Use d3.text and d3.csv.parseRows so that we do not need to have a header
 // row, and can receive the csv as an array of arrays.
 d3.text("visit-sequences.csv", function(text) {
-        var csv = d3.csvParseRows(text);
+        var csv = d3.csv.parseRows(text);
         var json = buildHierarchy(csv);
         console.log(json);
         createVisualization(json);
@@ -60,15 +61,10 @@ function createVisualization(json) {
         .attr("r", radius)
         .style("opacity", 0);
 
-    // Turn the data into a d3 hierarchy and calculate the sums.
-    var root = d3.hierarchy(json)
-        .sum(function(d) { return d.size; })
-        .sort(function(a, b) { return b.value - a.value; });
-  
     // For efficiency, filter nodes to keep only those large enough to see.
-    var nodes = partition(root).descendants()
+    var nodes = partition.nodes(json)
         .filter(function(d) {
-                return (d.x1 - d.x0 > 0.005); // 0.005 radians = 0.29 degrees
+                return (d.dx > 0.005); // 0.005 radians = 0.29 degrees
             });
 
     var path = vis.data([json]).selectAll("path")
@@ -77,7 +73,7 @@ function createVisualization(json) {
         .attr("display", function(d) { return d.depth ? null : "none"; })
         .attr("d", arc)
         .attr("fill-rule", "evenodd")
-        .style("fill", function(d) { return colors[d.data.name]; })
+        .style("fill", function(d) { return colors[d.name]; })
         .style("opacity", 1)
         .on("mouseover", mouseover);
 
@@ -85,7 +81,7 @@ function createVisualization(json) {
     d3.select("#container").on("mouseleave", mouseleave);
 
     // Get total size of the tree = value of root node from partition.
-    totalSize = path.datum().value;
+    totalSize = path.node().__data__.value;
 };
 
 // Fade all but the current sequence, and show it in the breadcrumb trail.
@@ -103,8 +99,7 @@ function mouseover(d) {
     d3.select("#explanation")
         .style("visibility", "");
 
-    var sequenceArray = d.ancestors().reverse();
-    sequenceArray.shift(); // remove root node from the array
+    var sequenceArray = getAncestors(d);
     updateBreadcrumbs(sequenceArray, percentageString);
 
     // Fade all the segments.
@@ -134,12 +129,24 @@ function mouseleave(d) {
         .transition()
         .duration(1000)
         .style("opacity", 1)
-        .on("end", function() {
+        .each("end", function() {
                 d3.select(this).on("mouseover", mouseover);
             });
 
     d3.select("#explanation")
         .style("visibility", "hidden");
+}
+
+// Given a node in a partition layout, return an array of all of its ancestor
+// nodes, highest first, but excluding the root.
+function getAncestors(node) {
+    var path = [];
+    var current = node;
+    while (current.parent) {
+        path.unshift(current);
+        current = current.parent;
+    }
+    return path;
 }
 
 function initializeBreadcrumbTrail() {
@@ -172,31 +179,31 @@ function breadcrumbPoints(d, i) {
 function updateBreadcrumbs(nodeArray, percentageString) {
 
     // Data join; key function combines name and depth (= position in sequence).
-    var trail = d3.select("#trail")
+    var g = d3.select("#trail")
         .selectAll("g")
-        .data(nodeArray, function(d) { return d.data.name + d.depth; });
-
-    // Remove exiting nodes.
-    trail.exit().remove();
+        .data(nodeArray, function(d) { return d.name + d.depth; });
 
     // Add breadcrumb and label for entering nodes.
-    var entering = trail.enter().append("svg:g");
+    var entering = g.enter().append("svg:g");
 
     entering.append("svg:polygon")
         .attr("points", breadcrumbPoints)
-        .style("fill", function(d) { return colors[d.data.name]; });
+        .style("fill", function(d) { return colors[d.name]; });
 
     entering.append("svg:text")
         .attr("x", (b.w + b.t) / 2)
         .attr("y", b.h / 2)
         .attr("dy", "0.35em")
         .attr("text-anchor", "middle")
-        .text(function(d) { return d.data.name; });
+        .text(function(d) { return d.name; });
 
-    // Merge enter and update selections; set position for all nodes.
-    entering.merge(trail).attr("transform", function(d, i) {
+    // Set position for entering and updating nodes.
+    g.attr("transform", function(d, i) {
             return "translate(" + i * (b.w + b.s) + ", 0)";
         });
+
+    // Remove exiting nodes.
+    g.exit().remove();
 
     // Now move and update the percentage at the end.
     d3.select("#trail").select("#endlabel")
